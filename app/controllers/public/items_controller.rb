d2b2a029eb7dc1677aa39b1f420324d869524b96
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-14 14:08:06 +0000

    [Add] ジャンル検索

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
     if params[:search]
       @items = Item.where("name LIKE ? ", '%' + params[:search] + '%')
+      @items = @items.page(params[:page])
+    elsif params[:genre_name]
+      @items = Item.where(params[:genre_name])
       @items = @items.page(params[:page])
     else
       @items = Item.page(params[:page])
