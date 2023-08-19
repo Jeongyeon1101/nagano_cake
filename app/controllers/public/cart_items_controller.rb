a1a750db2e961851502e1da9d22ad084ef53e4e9
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-19 11:49:26 +0000

    [Finish] カート追加（同一商品の数量変更）

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,18 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   def create
-    if CartItem.find_by(item_id: params[:item_id])
-      @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
-      @cart_item.customer_id = current_customer.id
-      @cart_item.item_id = @cart_item.item.id
-      @cart_item.amount = @cart_item.amount + params[:amount]
-      @cart_item.update(cart_item_params)
-      redirect_to cart_items_path
-    else
-      @cart_item = CartItem.new(cart_item_params)
-      @cart_item.customer_id = current_customer.id
-      @cart_item.item_id = @cart_item.item.id
-      @cart_item.save
-      redirect_to cart_items_path
-    end
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = @cart_item.item.id
+      if cart_item = @cart_items.find_by(item_id: params[:cart_item][:item_id])
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+      else
+        @cart_item.save
+      end
+    redirect_to cart_items_path
   end
 
   def index
