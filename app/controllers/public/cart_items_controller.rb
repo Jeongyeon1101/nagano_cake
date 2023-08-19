4d2a0b2c84f001fc096601bf607c255159665967
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-19 06:02:38 +0000

    [Add] カート内数量変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,20 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.item_id = @cart_item.item.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if CartItem.find_by(item_id: params[:item_id])
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+      @cart_item.customer_id = current_customer.id
+      @cart_item.item_id = @cart_item.item.id
+      @cart_item.amount = @cart_item.amount + params[:amount]
+      @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.item_id = @cart_item.item.id
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def index
