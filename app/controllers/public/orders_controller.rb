5f6228a72d6ebc43513868e2329f612bf7619f7c
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-24 14:22:48 +0000

    [Add] 注文確認画面 hidden_fieldを除く

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,12 +6,24 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    binding.pry
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items
+    @order.total_payment = 0
+    @order.shipping_cost = 800
+    if params[:order][:select_address] == "registered_address"
       @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
+    elsif params[:order][:select_address] == "current_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    else
+      @order.save
+    end
+
   end
 
   private
