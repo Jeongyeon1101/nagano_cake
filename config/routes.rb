e2d94bed1c9aebf1f4cab05399d13b73398b6346
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-28 14:11:20 +0000

    [Finish] admin 注文履歴詳細作成

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
 
   scope module: :public do
