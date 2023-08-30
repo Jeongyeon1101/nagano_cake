9235b5f76fb013eacc32eabcb52556780daa90b3
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-30 11:12:30 +0000

    [Finish]admin 注文ステータス更新処理(if文を除く)作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,17 @@ class Admin::OrdersController < ApplicationController
     @order_details = OrderDetail.where(order_id: params[:id])
     @order = Order.find(params[:id])
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
