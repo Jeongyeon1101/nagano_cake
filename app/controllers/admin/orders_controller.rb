6b91bf0ea19bb31ae5d3d4ece70df9a73dc2c67e
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-08-31 11:55:10 +0000

    [Finish] 注文・製作ステータス(if文を含む)作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
   def show
     @order_details = OrderDetail.where(order_id: params[:id])
     @order = Order.find(params[:id])
@@ -7,7 +8,9 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order_details = @order.order_details
-    @order.update(order_params)
+    if @order.update(order_params)
+      @order_details.update_all(making_status: 1) if  @order.status == "confirm_payment"
+    end
     redirect_to admin_order_path(@order)
   end
 
