e46a66cd2f26750607d43f4faa2a5dfaff4db0e8
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-09-02 05:35:02 +0000

    [Finish]cartitem更新時の不具合修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,8 +21,8 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    @cart_items = current_customer.cart_items
-    @cart_items.update(cart_item_params)
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
 
