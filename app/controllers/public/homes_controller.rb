f646f6506592402a70939abdda952239a0d34594
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-09-02 00:41:29 +0000

    [Finish] topページ修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,17 @@
 class Public::HomesController < ApplicationController
   def top
+     @genres = Genre.all
+    if params[:search]
+      @items = Item.where("name LIKE ? ", '%' + params[:search] + '%')
+      @items = @items.page(params[:page])
+      @genre_name = "商品"
+    elsif params[:genre_name]
+      @items = Genre.find_by(name: params[:genre_name]).items
+      @items = @items.page(params[:page])
+      @genre_name = params[:genre_name]
+    else
+      @items = Item.all.first(4)
+    end
   end
 
   def about
