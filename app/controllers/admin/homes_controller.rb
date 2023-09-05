a4c219d7bebbeb6e86b8791be1d857fa80539747
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-09-05 15:58:06 +0000

    [Add]レイアウト修正　rest customer/items

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,14 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @order_details = OrderDetail.page(params[:page])
+    if params[:customer_id]
+      @order_details = Customer.find_by(id: params[:customer_id]).orders
+      @order_details = @order_details.each do |order|
+      @order_details = @order_details.order_details  
+      @order_details = @order_details.page(params[:page])
+      @customer = Customer.find(params[:id])
+    else
+      @order_details = OrderDetail.page(params[:page])
+    end
   end
 end
