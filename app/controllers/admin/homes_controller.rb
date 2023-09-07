8b3ae9c0bb89d181b93d7c94dfb48a5857dad16f
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-09-07 15:57:21 +0000

    [Finish]レイアウト最終調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,14 +2,16 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     if params[:customer_id]
-      @orders = Customer.find_by(id: params[:customer_id]).orders
-      @order = @orders.each do |order|
-        @order_details = order.order_details
-      end
-      @order_details = @order_details.page(params[:page]).per(10)
-      @customer = Customer.find_by(id: params[:customer_id])
+      @customer = Customer.find(params[:customer_id])
+      @orders = @customer.orders.page(params[:page]).reverse_order
+      #@orders = Customer.find_by(id: params[:customer_id]).orders
+      #@orders.each do |order|
+        #@order_details = order.order_details
+      #end
+      #@order_details = @order_details.page(params[:page])
+      #@customer = Customer.find_by(id: params[:customer_id])
     else
-      @order_details = OrderDetail.page(params[:page]).per(10)
+      @order_details = OrderDetail.page(params[:page])
     end
   end
 end
